416be3d5673acf047436cc174f82b3d00b16f7e0
damirqa <damirshaniyazov@gmail.com>
2022-03-08 02:47:39 +0500

    rm bidirs deps

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { toast } from 'react-toastify';
- 
+import auth from '../services/authService';
+
 axios.interceptors.response.use(null, error => {
     const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
     
@@ -11,9 +12,14 @@ axios.interceptors.response.use(null, error => {
     return Promise.reject(error);
 });
 
+function setJwt(jwt) {
+  axios.defaults.headers.common['x-auth-token'] = jwt;
+}
+
 export default {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete: axios.delete
+    delete: axios.delete,
+    setJwt,
 }
