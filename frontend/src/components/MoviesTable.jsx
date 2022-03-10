d0701ac3c73985a6f64498f9368b2a6fc5fcb83f
damirqa <damirshaniyazov@gmail.com>
2022-03-10 13:42:36 +0500

    delete movies only for admin

--- frontend/src/components/MoviesTable.jsx
+++ frontend/src/components/MoviesTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Table from './table'; 
+import auth from '../services/authService';
 
 class MoviesTable extends Component {
     columns = [
@@ -9,8 +10,16 @@ class MoviesTable extends Component {
         {path: 'numberInStock', label: 'Stock'},
         {path: 'dailyRentalRate', label: 'Rate'},
         {key: 'like', content: movie => <i onClick={() => this.props.onFavorite(movie)} className={this.props.likedList.indexOf(movie._id) > -1 ? 'bi bi-heart-fill' : 'bi bi-heart' }></i>},
-        {key: 'delete', content: movie => <button onClick={() => this.props.onDelete(movie)} className='btn btn-danger btn-sm'>Delete</button>}
     ];
+
+    deleteColumn = {key: 'delete', content: movie => <button onClick={() => this.props.onDelete(movie)} className='btn btn-danger btn-sm'>Delete</button>}
+
+    constructor() {
+        super();
+    
+        const user = auth.getCurrentUser();
+        if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+    }
 
     render() { 
         const { movies, sortColumn, onSort } = this.props; 
